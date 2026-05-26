b3fa6ec761144817f9c8da6272399587c70a240a
Renan Oliveira <renanscudero@gmail.com>
2026-05-26 12:48:40 -0300

    ajuste vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['monitor.grupofuturanet.net.br'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
